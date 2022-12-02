e721d424c90bce570ead244038b62fc8626a4195
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2022-12-02 23:02:15 +0200

    ت

--- config/cache.php
+++ config/cache.php
@@ -66,7 +66,7 @@ return [
             ],
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', 'https://mansouraoutlet.herokuapp.com/'),
+                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
                     'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
