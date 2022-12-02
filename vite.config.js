530baee03909e4947bc13809e60e923ed616ff09
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2022-12-02 23:45:06 +0200

    ىثص رهفث

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,30 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    plugins: [
+        react(),
+        laravel({
+            input: [
+                'resources/js/app.jsx',
+                'resources/js/index.css',
+            ],
+            refresh: true,
+        }),
+
+    ],
+    outDir: 'public/build',
     build: {
         chunkSizeWarningLimit: 1600,
         // generate manifest.json in outDir
         manifest: true,
         rollupOptions: {
+            external: ['react'],
+            output:{
+                globals: {
+                    react: 'React'
+                  }
+            },
+          // overwrite default .html entry
           input: '/resources/js/app.jsx'
         }
       }
