fe1749b512dff7597643256c5555110a08041e48
Youssefmahmoud2022 <youssefwork2022develop@gmail.com>
2022-12-06 11:27:36 +0200

    build with right env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,13 @@
         <meta http-equiv='expires' content='0'>
         <meta http-equiv='pragma' content='no-cache'>
         <title>Mansoura Outlet</title>
-
+        <link rel="stylesheet" href="{{ mix('css/app.css') . "?id=" . Str::random(16) }}">
     </head>
 
     <body>
         <div id="app"></div>
-
-        <script src="{{ mix('js/app.js') }}"></script>
+ 
+        <script src="{{ mix('js/app.js') }}"></script> 
         <!-- <script src="resources/js/app.js"></script> -->
 
     </body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -28,23 +28,3 @@ mix.browserSync({
       ws: true,
     },
   });
-
-// mix.setPublicPath('/')
-// .webpackConfig({
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(jsx|js|vue)$/,
-//         loader: 'eslint-loader',
-//         enforce: 'pre',
-//         exclude: /(node_modules)/,
-//       }
-//     ]
-//   },
-// })
-// .options({
-//   hmrOptions: {
-//       port: 3000
-//   }
-// })
-// .react('src/index.js', '/dist/app.js');
