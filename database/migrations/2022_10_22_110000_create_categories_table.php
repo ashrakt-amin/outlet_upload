acf6e8fcca0419d35345a61711b50956991b38d1
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2022-12-07 16:50:29 +0200

    categories same table

--- database/migrations/2022_10_22_110000_create_categories_table.php
+++ database/migrations/2022_10_22_110000_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('category_id')->default(0);
+            $table->bigInteger('category_id')->nullable()->default(0);
             $table->timestamps();
         });
     }
