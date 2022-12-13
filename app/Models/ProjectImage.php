3830c5cb408157d933f59972d8b9aae5e4a13f7b
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2022-12-13 13:39:43 +0200

    d

--- app/Models/ProjectImage.php
+++ app/Models/ProjectImage.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectImage extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
 }
