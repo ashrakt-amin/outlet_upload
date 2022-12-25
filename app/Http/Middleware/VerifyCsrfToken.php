e7c567b7d8e20c63bd0008a9efe378dd50a5376a
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2022-12-25 11:57:30 +0200

    update middlware cors

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,8 +11,5 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [
-        'api/*', 
-        'sanctum/csrf-cookie'
-    ];
+    protected $except = ['api/*', 'sanctum/csrf-cookie'];
 }
--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -21,6 +21,7 @@ class Cors
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+        ->header('content-type: application/json; charset=utf-8')
         ->header('Access-Control-Allow-Credentials',' true');
     }
 }
