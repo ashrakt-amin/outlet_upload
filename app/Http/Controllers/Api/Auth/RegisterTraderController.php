50a64ae7b19a43ca50c5099f24eb1cc6031ae110
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2022-12-26 13:26:16 +0200

    reponse Traite

--- app/Http/Controllers/Api/Auth/RegisterTraderController.php
+++ app/Http/Controllers/Api/Auth/RegisterTraderController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Models\Trader;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\BaseController as BaseController;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\TraderResource;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Traits\ResponseTrait as TraitResponseTrait;
 use App\Http\Traits\AuthGuardTrait as TraitsAuthGuardTrait;
+use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Http\Traits\ImageProccessingTrait as TraitImageProccessingTrait;
 
 
-class RegisterTraderController extends BaseController
+class RegisterTraderController extends Controller
 {
     use TraitsAuthGuardTrait;
     use TraitImageProccessingTrait;
+    use TraitResponseTrait;
     /**
      * Register api
      *
@@ -21,19 +25,25 @@ class RegisterTraderController extends BaseController
      */
     public function register(Request $request)
     {
-        // $request->validate([
-        //     'phone' => 'unique:traders,phone|regex:/^(01)[0-9]{9}$/',
-        // ], [
-        //     // 'phone.required' => 'الهاتف مسجل من قبل',
-        //     'phone.unique' => 'الهاتف مسجل من قبل',
-        //     'phone.regex' => 'صيغة الهاتف غير صحيحة',
-        // ]);
-        $trader = Trader::where(['phone'=>$request->input('phone')])->first();
-        if ($trader != null) {
-            return response()->json([
-                'message' => 'الهاتف مسجل من قبل',
-            ], 422);
-        }
+        $validator = Validator::make($request->all(), [
+            'phone'            => 'unique:traders,phone|regex:/^(01)[0-9]{9}$/',
+            'email'            => 'unique:traders,email',
+            'password'         => 'required:traders,email',
+            'f_name'           => 'required',
+        ], [
+            'email.unique'     => 'البريد الالكتروني مسجل من قبل',
+            'phone.unique'     => 'الهاتف مسجل من قبل',
+            'phone.required'   => 'الهاتف مطلوب',
+            'phone.regex'      => 'يرجى التاكد ان الهاتف صحيحا',
+            'f_name.required'  => 'الاسم الاول مطلوب',
+            'password.required'=> 'الرقم السري مطلوب',
+        ]);
+        // $trader = Trader::where(['phone'=>$request->input('phone')])->first();
+        // if ($trader != null) {
+        //     return response()->json([
+        //         'message' => 'الهاتف مسجل من قبل',
+        //     ], 422);
+        // }
         // if ($request->input('code')) {
         //     $user = Trader::where(['code'=>$request->input('code')])->first();
         //     $age = $user->age;
@@ -47,8 +57,10 @@ class RegisterTraderController extends BaseController
                         $user = new Trader();
                         $user->fill($request->input());
                         if ($request->has('img')) {
-                            $img = $request->file('img');
-                            $user->img = $this->setImage($img, 'traders', 450, 450);
+                            if ($request->file('img') != null) {
+                                $img = $request->file('img');
+                                $user->img = $this->setImage($img, 'traders', 450, 450);
+                            }
                         }
 
                         $user->password = bcrypt($request->password);
@@ -64,8 +76,8 @@ class RegisterTraderController extends BaseController
                         // $user->password = $input['password'];
                         // $user->updatesa();
                         if ($user->save()) {
-                            $success['token']     =  $user->createToken('trader')->plainTextToken;
-                            $success['tokenName'] =  "trader";
+                            // $success['token']     =  $user->createToken('trader')->plainTextToken;
+                            // $success['tokenName'] =  "trader";
                             $success['name']      =  new TraderResource($user);
                             return $this->sendResponse($success, 'تم التسجيل بنجاح.');
                         }
