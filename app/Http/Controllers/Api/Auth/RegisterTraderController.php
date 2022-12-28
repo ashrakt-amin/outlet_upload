c28d891f671d20fb302be5516fa89b3bd2c6fe89
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2022-12-28 11:25:14 +0200

    validate register trader

--- app/Http/Controllers/Api/Auth/RegisterTraderController.php
+++ app/Http/Controllers/Api/Auth/RegisterTraderController.php
@@ -37,61 +37,32 @@ class RegisterTraderController extends Controller
             'f_name.required'  => 'الاسم الاول مطلوب',
             'password.required'=> 'الرقم السري مطلوب',
         ]);
-        // $trader = Trader::where(['phone'=>$request->input('phone')])->first();
-        // if ($trader != null) {
-        //     return response()->json([
-        //         'message' => 'الهاتف مسجل من قبل',
-        //     ], 422);
-        // }
-        // if ($request->input('code')) {
-        //     $user = Trader::where(['code'=>$request->input('code')])->first();
-        //     $age = $user->age;
-        //     if ($user) {
-        //         if ($user->phone == $request->input('phone')) {
-                    if ($request->input('password') !== $request->input('confirm_password')) {
-                        return response()->json([
-                            'message' => 'الرقم السري غير مطابق',
-                        ], 422);
-                    } else {
-                        $user = new Trader();
-                        $user->fill($request->input());
-                        if ($request->has('img')) {
-                            if ($request->file('img') != null) {
-                                $img = $request->file('img');
-                                $user->img = $this->setImage($img, 'traders', 450, 450);
-                            }
-                        }
 
-                        $user->password = bcrypt($request->password);
-                        $user->code = randomCode();
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        
+        if ($request->input('password') !== $request->input('confirm_password')) {
+            return response()->json([
+                'message' => 'الرقم السري غير مطابق',
+            ], 422);
+        } else {
+            $user = new Trader();
+            $user->fill($request->input());
+            if ($request->has('img')) {
+                if ($request->file('img') != null) {
+                    $img = $request->file('img');
+                    $user->img = $this->setImage($img, 'traders', 450, 450);
+                }
+            }
 
-                        // if ($request->age != null) {
-                        //     $user->age = $request->age;
-                        // } else {
-                        //     $user->age = $age;
-                        // }
-                        // $input = $request->all();
-                        // $input['password'] = bcrypt($input['password']);
-                        // $user->password = $input['password'];
-                        // $user->updatesa();
-                        if ($user->save()) {
-                            // $success['token']     =  $user->createToken('trader')->plainTextToken;
-                            // $success['tokenName'] =  "trader";
-                            $success['data']      =  new TraderResource($user);
-                            return $this->sendResponse($success, 'تم التسجيل بنجاح.');
-                        }
-                    }
-            //     } else {
-            //         return response()->json([
-            //             'message' => 'الهاتف غير صحيح',
-            //         ], 422);
-            //     }
-            // } else {
-            //     return response()->json([
-            //         'message' => 'لا يوجد تاجر بهذا الكود',
-            //     ], 422);
-            // }
-        // }
+            $user->password = bcrypt($request->password);
+            $user->code = randomCode();
+            if ($user->save()) {
+                $success['data']      =  new TraderResource($user);
+                return $this->sendResponse($success, 'تم التسجيل بنجاح.');
+            }
+        }
     }
 
 }
