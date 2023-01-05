62ee21abe37f7dc50113288faf881466ab2fd52f
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2023-01-05 14:58:54 +0200

    cATEGO UNIT TABLE

---
+++ database/migrations/2023_01_04_084220_create_category_unit_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_unit', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('unit_id');
+            $table->unsignedBigInteger('trader_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_unit');
+    }
+};
