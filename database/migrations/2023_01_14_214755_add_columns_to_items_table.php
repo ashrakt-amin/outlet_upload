c8bb9858f0ebbd5c8fa4d7bed4e0b236ae52c204
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2023-01-15 10:33:19 +0200

    add & edit migration

---
+++ database/migrations/2023_01_14_214755_add_columns_to_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->unsignedBigInteger('level_id')->nullable();
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->boolean('flash_sales')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropColumn('level_id');
+            $table->dropColumn('project_id');
+            $table->dropColumn('flash_sales');
+        });
+    }
+};
