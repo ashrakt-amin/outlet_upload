faf02c7784e2dcd0f24f8b6def43cc7e1117e939
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2023-01-20 00:05:05 +0200

    error cat_id in AllConditions method

--- app/Repository/Eloquent/ItemRepository.php
+++ app/Repository/Eloquent/ItemRepository.php
@@ -65,13 +65,13 @@ class ItemRepository extends BaseRepository implements ItemRepositoryInterface
      */
     public function itemsWhereDiscountForAllConditions(array $attributes): Collection
     {
-        $categories = Category::where(['parent_id' =>  $attributes['category_id']])->pluck('id')->all();
+        $categories = array_key_exists('category_id', $attributes) ? Category::where(['parent_id' =>  $attributes['category_id']])->pluck('id')->all() : '';
         return $this->model->where(function($q) use($attributes){
             !array_key_exists('columnName', $attributes) || $attributes['columnValue'] == 0  ?: $q
             ->where([$attributes['columnName'] => $attributes['columnValue']]);
             })
             ->where(function($q) use($attributes, $categories){
-                !array_key_exists('category_id', $attributes)|| $attributes['category_id'] == 0   ?: $q
+                !array_key_exists('category_id', $attributes) || $attributes['category_id'] == 0   ?: $q
                 ->whereIn('category_id', $categories);
         })->where('discount', '>', 0)->get();
     }
