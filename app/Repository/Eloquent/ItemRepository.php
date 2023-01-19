eedc232d423623de5af15fd9a5086bb141b55992
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2023-01-19 16:59:24 +0200

    itemWhereDiscountForAllConditions & categories

--- app/Repository/Eloquent/ItemRepository.php
+++ app/Repository/Eloquent/ItemRepository.php
@@ -66,7 +66,12 @@ class ItemRepository extends BaseRepository implements ItemRepositoryInterface
     public function itemWhereDiscountForAllConditions(array $attributes): Collection
     {
         return $this->model->where(function($q) use($attributes){
-            !array_key_exists('columnName', $attributes) || $attributes['columnName'] == 0  ?: $q->where([$attributes['columnName'] => $attributes['columnValue']]);
+            !array_key_exists('columnName', $attributes) || $attributes['columnName'] == 0  ?: $q
+            ->where([$attributes['columnName'] => $attributes['columnValue']]);
+            })
+            ->where(function($q) use($attributes){
+                !array_key_exists('category_id', $attributes) ?: $q
+                ->where(['category_id' => $attributes['category_id']]);
         })->where('discount', '>', 0)->get();
     }
 
