453704292285273ab1f8b67bb3ecbd0fa08ec390
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2023-02-07 11:00:47 +0200

    "تم تعديل الاعلان"

--- app/Http/Controllers/Api/AdvertisementController.php
+++ app/Http/Controllers/Api/AdvertisementController.php
@@ -104,7 +104,7 @@ class AdvertisementController extends Controller
      */
     public function show(Advertisement $advertisement)
     {
-        return new AdvertisementResource($this->advertisementRepository->find($advertisement->id));
+        return $this->sendResponse(new AdvertisementResource($this->advertisementRepository->find($advertisement->id)), "", 200);
     }
 
     /**
@@ -116,7 +116,7 @@ class AdvertisementController extends Controller
      */
     public function update(Request $request, Advertisement $advertisement)
     {
-        return new AdvertisementResource($this->advertisementRepository->edit($advertisement->id, $request->all()));
+        return $this->sendResponse(new AdvertisementResource($this->advertisementRepository->edit($advertisement->id, $request->all())), "تم التعديل", 200);
 
 
         // $current_expire = strtotime($advertisement->advertisement_expire);
